9556b1f75af882091e3b49b4ef4f0abbf3c1a1fc
sumarius56 <110302280+sumarius56@users.noreply.github.com>
2022-08-23 21:27:41 +0300

    debugged

--- best-eats/src/components/Footer.jsx
+++ best-eats/src/components/Footer.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' bg-orange-600  w-full h-20 m-auto flex justify-between items-center text-white text-xs  '>
+    <div className=' bg-orange-600 max-w-[1640px] w-full p-4  text-white grid grid-cols-2 sm:flex gap-6 justify-around items=center m-auto'>
 
-      <div className='px-20 m-1'>
-       <p className='p-1 '>Contact</p>
-       <p className='p-1 '>About</p>
-       <p className='p-1 '>Ideeas</p>
+      <div className='  '>
+       <p className=' '>Contact</p>
+       <p className=' '>About</p>
+       <p className=' '>Ideeas</p>
       </div>
 
-       <div className='px-20 py-1 m-1'>
-        <p className='p-1 '>Complaints</p>
-        <p className='p-1 '>Meal of the day</p>
-        <p className='p-1 '>Where are we located?</p>
+       <div className=' '>
+        <p className=' '>Complaints</p>
+        <p className=' '>Meal of the day</p>
+        <p className=' '>Where are we located?</p>
        </div>
 
-     <div className='px-20 py-1 m-1'>
-      <p className='p-1 '>I want to eat ....</p>
-      <p className='p-1 '>Discontinued Products </p>
-      <p className='p-1 '>En gros orders</p>
+     <div className='  '>
+      <p className=' '>I want to eat ....</p>
+      <p className=' '>Discontinued Products </p>
+      <p className=' '>En gros orders</p>
      </div>
 
 
-     <div className='px-20 py-1 m-1'>
-      <p className='p-1 '>Have you tried the snails yet?</p>
-      <p className='p-1 '>Future Discounts</p>
-      <p className='p-1 '>Out of frog legs!</p>
+     <div className=' '>
+      <p className=' '>Tried the snails yet?</p>
+      <p className=' '>Future Discounts</p>
+      <p className=' '>Out of frog legs!</p>
      </div>
 
 
